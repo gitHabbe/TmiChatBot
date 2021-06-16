485bbaf78034c8dde39991565f425652bb2c4ff2
Habbe <91habbe@gmail.com>
2021-06-16 18:33:30 +0200

    ✨Add: boilerplate command definition

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Client, ChatUserstate } from "tmi.js";
 import { tmiOptions } from "./config/tmiConfig";
+import { CommandName } from "./interfaces/tmi";
 
 const tmiClient = new Client(tmiOptions);
 
@@ -10,11 +11,21 @@ try {
   console.log(`TMI Connect error: ${error}`);
 }
 
-tmiClient.on(
-  "message",
-  (channel: string, tags: ChatUserstate, message: string, self: boolean) => {
-    if (self) return;
+tmiClient.on("message", (channel, userstate, message, self) => {
+  if (self) return;
+  const isCommand: boolean = message.startsWith("!");
+  if (isCommand) return;
 
-    const firstWord = message.startsWith("!");
+  const userCommandName: string = message.split(" ")[0].slice(1).toUpperCase();
+  switch (userCommandName) {
+    case CommandName.WR:
+      console.log("WR");
+      break;
+    case CommandName.PB:
+      console.log("PB");
+      break;
+
+    default:
+      break;
   }
-);
+});
