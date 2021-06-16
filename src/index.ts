5e3b581512ced05ea604e3741b3c643051c81b7b
Habbe <91habbe@gmail.com>
2021-06-16 17:17:09 +0200

    ✨Add: command boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import { Client } from "tmi.js";
+import { Client, ChatUserstate } from "tmi.js";
 import { tmiOptions } from "./config/tmiConfig";
 
 const tmiClient = new Client(tmiOptions);
 
-tmiClient.connect();
+try {
+  tmiClient.connect();
+  console.log("TMI logged in!");
+} catch (error) {
+  console.log(`TMI Connect error: ${error}`);
+}
+
+tmiClient.on(
+  "message",
+  (channel: string, tags: ChatUserstate, message: string, self: boolean) => {
+    if (self) return;
+
+    const firstWord = message.startsWith("!");
+  }
+);
