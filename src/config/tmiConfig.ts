f7280729a2925fc67c9cb6e5d45dae678c3f6e67
Habbe <91habbe@gmail.com>
2021-06-16 15:08:02 +0200

    🔧Config: successfully logs in

--- src/config/tmiConfig.ts
+++ src/config/tmiConfig.ts
@@ -6,5 +6,15 @@ dotenv.config();
 export const tmiOptions: Options = {
   options: {
     clientId: process.env.TWITCH_CLIENT_ID,
+    debug: true,
   },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+  identity: {
+    username: process.env.TWITCH_USERNAME,
+    password: "oauth:" + process.env.TWITCH_OAUTH_PASSWORD,
+  },
+  channels: ["habbe"],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { Client } from "tmi.js";
+import { tmiOptions } from "./config/tmiConfig";
 
-const TmiClient = new Client({});
+const tmiClient = new Client(tmiOptions);
+
+tmiClient.connect();
