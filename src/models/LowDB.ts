3baa3af26a68d3af1f101ad6165b08e5d7dc7181
Habbe <91habbe@gmail.com>
2021-07-02 20:54:28 +0200

    🔀Branch Merge: lowdb test

--- src/models/LowDB.ts
+++ src/models/LowDB.ts
@@ -1,55 +1,57 @@
 import { join } from "path";
 import { chain, PrimitiveChain } from "lodash";
-import { Adapter, JSONFile, Low, SyncAdapter, JSONFileSync } from "lowdb";
+import { Adapter, JSONFile, Low, SyncAdapter, JSONFileSync, LowSync, lowdb } from "lowdb";
 import { TSpeedgame } from "../interfaces/lowdb";
 
 // chain([1,2,3])
 // chain()
 
-type asdf = {
-  names: string[];
-  chain: string[];
-};
+// type asdf = {
+//   names: string[];
+//   chain: typeof chain;
+// };
+
+class LowDBWithChain<T> extends Low<T> {
+  chain: PrimitiveChain<T | null>;
+  // private data2: T[] = [];
+  constructor(adapter: Adapter<T>) {
+    super(adapter)
+    this.chain = chain(this.data);
+  }
+
+  // createChain = (data: <T>[]) => {
+  //   this.chain = chain(data);
+  // }
+}
 class LowDB<T> {
-  private data: T[] = [];
+  // private data: T[] = [];
+  private json: string;
   private adapter: JSONFile<T>;
   private lowdb: Low<T>;
-  private json: string;
   // private asdf: PrimitiveChain<T> = [];
   // private db: typeof chain;
 
   constructor(dbName: string) {
     this.json = join(__dirname + "/private/", dbName);
     this.adapter = new JSONFile<T>(this.json);
-    this.lowdb = new Low<T>(this.adapter);
+    this.lowdb = new LowDBWithChain<T>(this.adapter);
   }
 
   init = async () => {
     await this.lowdb.read();
-    // this.asdf = chain(this.lowdb.data)
+    this.lowdb.
+    // this. 
+    const asdf = chain(this.lowdb.data);
     return chain(this.lowdb.data);
   };
 
   getById = (id: string) => {
     const db = this.init();
+    this.lowdb
     return db;
   };
 
-  getData = () => {
-    return this.data;
-  };
+  // getData = () => {
+  //   return this.data;
+  // };
 }
-
-// const asdf = new LowDB<asdf>("game_database.json");
-// const asdf2 = asdf.getById("a");
-// asdf2.then(data => data.)
-// console.log(asdf);
-
-const aa = async () => {
-  const adapter = new JSONFile<asdf>(
-    join(__dirname + "/private/", "game_database.json")
-  );
-  const asdf2 = new Low(adapter);
-  await asdf2.adapter.read();
-  asdf2.data.chain = chain<asdf>(asdf2.data);
-};
