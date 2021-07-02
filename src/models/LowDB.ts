0beb79afc255dca0de48ba530ffdb74ad3c889d0
Habbe <91habbe@gmail.com>
2021-07-02 20:46:57 +0200

    ✨Add: lodash lowdb support

--- src/models/LowDB.ts
+++ src/models/LowDB.ts
@@ -1,20 +1,55 @@
-import { Adapter, JSONFile, Low } from "lowdb/lib";
-import { join } from "path/posix";
-import { StringDecoder } from "string_decoder";
+import { join } from "path";
+import { chain, PrimitiveChain } from "lodash";
+import { Adapter, JSONFile, Low, SyncAdapter, JSONFileSync } from "lowdb";
 import { TSpeedgame } from "../interfaces/lowdb";
 
-class LowDB<T> {
-  private adapter: JSONFile<T>;
-  private json: string;
-  private db: Low<T>;
+// chain([1,2,3])
+// chain()
 
-  constructor(private dbName: string) {
-    this.json = join(__dirname, this.dbName);
+type asdf = {
+  names: string[];
+  chain: string[];
+};
+class LowDB<T> {
+  private data: T[] = [];
+  private adapter: JSONFile<T>;
+  private lowdb: Low<T>;
+  private json: string;
+  // private asdf: PrimitiveChain<T> = [];
+  // private db: typeof chain;
+
+  constructor(dbName: string) {
+    this.json = join(__dirname + "/private/", dbName);
     this.adapter = new JSONFile<T>(this.json);
-    this.db = new Low<T>(this.adapter);
+    this.lowdb = new Low<T>(this.adapter);
   }
 
-  getById = (id: string): void => {
-    this.db;
+  init = async () => {
+    await this.lowdb.read();
+    // this.asdf = chain(this.lowdb.data)
+    return chain(this.lowdb.data);
+  };
+
+  getById = (id: string) => {
+    const db = this.init();
+    return db;
+  };
+
+  getData = () => {
+    return this.data;
   };
 }
+
+// const asdf = new LowDB<asdf>("game_database.json");
+// const asdf2 = asdf.getById("a");
+// asdf2.then(data => data.)
+// console.log(asdf);
+
+const aa = async () => {
+  const adapter = new JSONFile<asdf>(
+    join(__dirname + "/private/", "game_database.json")
+  );
+  const asdf2 = new Low(adapter);
+  await asdf2.adapter.read();
+  asdf2.data.chain = chain<asdf>(asdf2.data);
+};
