e4aaeaac9ab0ce5d802f6c4114e39d15abced7bf
Habbe <91habbe@gmail.com>
2021-07-02 21:17:10 +0200

    ✨Add: lowdb first draft

--- src/models/LowDB.ts
+++ src/models/LowDB.ts
@@ -1,33 +1,51 @@
 import { join } from "path";
 import { chain, PrimitiveChain } from "lodash";
-import { Adapter, JSONFile, Low, SyncAdapter, JSONFileSync, LowSync, lowdb } from "lowdb";
+import {
+  Adapter,
+  JSONFile,
+  Low,
+  SyncAdapter,
+  JSONFileSync,
+  LowSync,
+} from "lowdb";
 import { TSpeedgame } from "../interfaces/lowdb";
 
 // chain([1,2,3])
 // chain()
 
-// type asdf = {
-//   names: string[];
-//   chain: typeof chain;
-// };
+type asdf = {
+  names: string[];
+  // chain: typeof chain;
+};
 
 class LowDBWithChain<T> extends Low<T> {
   chain: PrimitiveChain<T | null>;
   // private data2: T[] = [];
   constructor(adapter: Adapter<T>) {
-    super(adapter)
+    super(adapter);
     this.chain = chain(this.data);
+    console.log("RAN THIS");
   }
 
   // createChain = (data: <T>[]) => {
   //   this.chain = chain(data);
   // }
 }
+
+// class bbb {
+//   bb: string = "ASDF";
+// }
+// class aaa extends bbb {
+//   constructor() {
+//     super()
+//     this.bb
+//   }
+// }
 class LowDB<T> {
   // private data: T[] = [];
   private json: string;
   private adapter: JSONFile<T>;
-  private lowdb: Low<T>;
+  private lowdb: LowDBWithChain<T>;
   // private asdf: PrimitiveChain<T> = [];
   // private db: typeof chain;
 
@@ -35,23 +53,28 @@ class LowDB<T> {
     this.json = join(__dirname + "/private/", dbName);
     this.adapter = new JSONFile<T>(this.json);
     this.lowdb = new LowDBWithChain<T>(this.adapter);
+    this.lowdb.read();
   }
 
-  init = async () => {
-    await this.lowdb.read();
-    this.lowdb.
-    // this. 
-    const asdf = chain(this.lowdb.data);
-    return chain(this.lowdb.data);
-  };
-
-  getById = (id: string) => {
-    const db = this.init();
-    this.lowdb
-    return db;
-  };
-
-  // getData = () => {
-  //   return this.data;
+  // init = async () => {
+  //   await this.lowdb.read();
+  //   // this.lowdb.chain
+  //   // this.
+  //   const asdf = chain(this.lowdb.data);
+  //   return chain(this.lowdb.data);
   // };
+
+  // getById = (id: string) => {
+  //   const db = this.init();
+  //   this.lowdb;
+  //   return db;
+  // };
+
+  getData = () => {
+    return this.lowdb.chain;
+  };
 }
+
+const asdf = new LowDB<asdf>("game_database.json");
+// asdf.init();
+asdf.getData();
