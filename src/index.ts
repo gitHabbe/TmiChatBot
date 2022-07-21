3141aeb04466921634b6aec124ec50f7eab68d66
Habbe <91habbe@gmail.com>
2022-07-21 10:55:31 +0200

    ✨ Add: Tmi events to seperate class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Tmi } from "./models/Tmi";
+import { Events } from "./models/Events";
 
+const tmi: Tmi = new Tmi();
+const events = new Events(tmi.client);
+tmi.addEvent("message", events.onMessage)
 try {
-  const tmi = new Tmi();
   tmi.connect()
 } catch (error) {
   console.log(`TMI Connect error: ${error}`);
--- src/models/Tmi.ts
+++ src/models/Tmi.ts
@@ -7,29 +7,30 @@ import { StandardCommand } from "./commands/StandardCommand";
 import { MessageData } from "./MessageData";
 
 export class Tmi {
-  private client: Client = new Client(tmiOptions);
+  client: Client = new Client(tmiOptions);
+  private events: {[name: string]: Function} = {};
 
   connect = () => {
-    this.client.connect().then(r => { console.log(r)} )
+    this.client.connect().then();
     this.client.on("message", this.onMessage)
   }
 
-  onMessage: OnMessage = async (
-      streamer: string,
-      chatter: ChatUserstate,
-      message: string,
-      self: boolean
-  ) => {
+  addEvent = (name: string, eventFunction: Function): void => {
+    this.events[name] = eventFunction;
+  }
+
+  onMessage: OnMessage = async ( streamer: string, chatter: ChatUserstate, message: string, self: boolean ): Promise<void> => {
     if (self) return;
     const messageData = new MessageData(streamer, chatter, message);
 
     try {
       const chatLink = new LinkCommand(messageData);
-      const linkInfo = await chatLink.print()
+      const linkInfo = await chatLink.run()
       await this.client.say(streamer, linkInfo)
     } catch (error) {
       return
     }
+
     try {
       const customCommand = new CustomCommand(messageData)
       const command = await customCommand.run();
