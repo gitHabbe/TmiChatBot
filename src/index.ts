7d9ab015d99e9464d128369b4a968b8afb5aeb7a
Habbe <91habbe@gmail.com>
2022-10-23 16:48:07 +0200

    🔥 Del: Tmi event handler

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { Tmi } from "./models/Tmi";
-import { Events } from "./models/Events";
 
-const tmi: Tmi = new Tmi();
-const events = new Events(tmi.client);
+const tmi = new Tmi();
 
-tmi.addEvent("message", events.onMessage)
 try {
   tmi.connect()
 } catch (error) {
--- src/models/Tmi.ts
+++ src/models/Tmi.ts
@@ -1,20 +1,56 @@
-import { Client } from "tmi.js";
+import { ChatUserstate, Client } from "tmi.js";
 import { tmiOptions } from "../config/tmiConfig";
+import { OnMessage } from "../interfaces/tmi";
+import { MessageData } from "./MessageData";
+import { StandardCommand } from "./commands/StandardCommand";
+import { CustomCommand } from "./commands/CustomCommand";
+import { LinkCommand } from "./commands/LinkCommand";
 
 export class Tmi {
-  private events: { [name: string]: Function } = {};
   client: Client;
 
   constructor(client: Client = new Client(tmiOptions)) {
     this.client = client;
+    this.addMessageEvent()
   }
 
   connect = () => {
     this.client.connect().then();
   }
 
-  addEvent = (name: string, eventFunction: Function): void => {
-    this.events[name] = eventFunction;
+  addMessageEvent = (): void => {
+    this.client.on("message", this.onMessage)
+  }
+
+  private onMessage: OnMessage = async (streamer: string, chatter: ChatUserstate, message: string, self: boolean) => {
+    if (self) return //"Bot self message";
+    try {
+      let messageData: MessageData = new MessageData(streamer, chatter, message);
+
+      const standardCommand = new StandardCommand(messageData, this.client);
+      messageData = await standardCommand.run();
+      console.log(messageData)
+      if (messageData.response.length > 0) {
+        return await this.client.say(messageData.targetChannel, messageData.response)
+      }
+      //
+      // const customCommand = new CustomCommand(messageData);
+      // messageData = await customCommand.run();
+      // if (messageData.response.length > 0) {
+      //   return await this.client.say(messageData.targetChannel, messageData.response)
+      // }
+      //
+      // const linkCommand = new LinkCommand(messageData);
+      // messageData = await linkCommand.run()
+      // if (messageData.response.length > 0) {
+      //   return await this.client.say(messageData.targetChannel, messageData.response)
+      // }
+      //
+      // return messageData
+    } catch (error) {
+      console.log(error)
+      console.log("No command was used")
+    }
   }
 }
 
