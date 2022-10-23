68ce3e2c20fbb9b5e020b2caf4ab3fba7e9d7381
Habbe <91habbe@gmail.com>
2022-10-23 16:49:41 +0200

    ♻️ Ref: support new impl

--- src/models/StringExtract.ts
+++ src/models/StringExtract.ts
@@ -1,31 +1,31 @@
 import { twitchAPI } from "../config/twitchConfig";
-import { IStreamerResponse, ITwitchChannel } from "../interfaces/twitch";
+import { IStreamerResponse } from "../interfaces/twitch";
 import { Api } from "./fetch/SpeedrunCom";
-import { TwitchChannel } from "./fetch/TwitchTv";
-import {MessageData} from "./MessageData";
+import { TwitchChannelApi } from "./fetch/TwitchTv";
+import { MessageData } from "./MessageData";
+import { TwitchFetch } from "./commands/Twitch";
 
 export class StringExtract {
+  private twitchFetch = new TwitchFetch(this.messageData);
   constructor(private messageData: MessageData) {}
 
   game = async (): Promise<string> => {
-    const { channel, message } = this.messageData;
+    const { message } = this.messageData;
     const messageArray = message.split(" ").slice(1);
     if (!messageArray || !messageArray[0]) {
-      const api = new Api<IStreamerResponse>(twitchAPI);
-      const twitchChannel = new TwitchChannel(api, channel);
-      const { game_name } = await twitchChannel.find();
+      const twitchChannels = await this.twitchFetch.getChannels()
+      const { game_name } = this.twitchFetch.filteredChannel(twitchChannels);
       return game_name;
     }
     return messageArray[0];
   };
 
   category = async (): Promise<string> => {
-    const { channel, message } = this.messageData;
+    const { message } = this.messageData;
     const messageArray = message.split(" ").slice(2);
     if (!messageArray || !messageArray[0]) {
-      const api = new Api<IStreamerResponse>(twitchAPI);
-      const twitchChannel = new TwitchChannel(api, channel);
-      const { title } = await twitchChannel.find();
+      const twitchChannels = await this.twitchFetch.getChannels();
+      const { title } = this.twitchFetch.filteredChannel(twitchChannels);
       return title;
     }
     return messageArray.join(" ");
