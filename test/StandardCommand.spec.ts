b66508a9b91aaa79ce97c789de7e6682588be864
Habbe <91habbe@gmail.com>
2022-10-31 15:30:04 +0100

    ✅ Test: 2 working tests for Uptime class

--- test/StandardCommand.spec.ts
+++ test/StandardCommand.spec.ts
@@ -1,20 +1,36 @@
 import { TwitchFetch, TwitchUptime } from "../src/models/commands/Twitch";
 import { ITwitchChannel } from "../src/interfaces/twitch";
-import { messageDataMock, twitchFetchMockData } from "./mockData";
+import { messageDataMock, offlineTwitchChannelMock, onlineTwitchChannelMock } from "./mockData";
+import { MessageData } from "../src/models/MessageData";
 
 describe("Standard commands", () => {
 
-    it("Uptime command", async () => {
+    it("Uptime channel online", async () => {
         const twitchFetch = new TwitchFetch(messageDataMock);
-        const spy = jest
-            .spyOn(twitchFetch, "getChannels")
-            .mockImplementation(async (): Promise<ITwitchChannel[]> => twitchFetchMockData)
-        const twitchUptime: TwitchUptime = new TwitchUptime(messageDataMock, twitchFetch);
-        const updatedMessageData = await twitchUptime.run();
+        const spy = mockGetChannelsMethod(twitchFetch, onlineTwitchChannelMock);
+        const twitchUptime = new TwitchUptime(messageDataMock, twitchFetch);
+        const updatedMessageData: MessageData = await twitchUptime.run();
         const response: string = updatedMessageData.response;
-        const exp: string = "habbe not online";
+        const exp = "2h 5m 34s";
         expect(response).toBe(exp)
         spy.mockRestore()
     })
+
+    it("Uptime channel offline", async () => {
+        const twitchFetch = new TwitchFetch(messageDataMock);
+        const spy = mockGetChannelsMethod(twitchFetch, offlineTwitchChannelMock);
+        const twitchUptime = new TwitchUptime(messageDataMock, twitchFetch);
+        const updatedMessageData: MessageData = await twitchUptime.run();
+        const response: string = updatedMessageData.response;
+        const exp = "habbe not online";
+        expect(response).toBe(exp)
+        spy.mockRestore()
+    })
+
+    function mockGetChannelsMethod(twitchFetch: TwitchFetch, twitchChannelMockData: ITwitchChannel[]) {
+        return jest
+            .spyOn(twitchFetch, "getChannels")
+            .mockImplementation(async (): Promise<ITwitchChannel[]> => twitchChannelMockData);
+    }
 
 })
