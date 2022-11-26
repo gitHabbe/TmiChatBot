47be836029a2f64f725145bca27e54d3d3b94802
Habbe <91habbe@gmail.com>
2022-11-26 18:12:36 +0100

    ✅ Test: extract mocking methods

--- test/SocialMedia.spec.ts
+++ test/SocialMedia.spec.ts
@@ -10,9 +10,7 @@ describe("Social media module", () => {
     const regexResult: RegExpExecArray | null = twitterRegex.exec(twitterTestString);
     if (!regexResult) return
     const twitterLink = new TwitterLink(regexResult);
-    const spy = jest
-      .spyOn(twitterLink, "fetchTweet")
-      .mockImplementation(async () => tweetMockData)
+    const spy = mockTweet(twitterLink);
     const response = await twitterLink.getMessage();
     const expected = "[Jesse Duffield@DuffieldJesse] New Lazygit release in stores now! https://t.co/tfgU5rtT2R";
     expect(response).toBe(expected)
@@ -24,13 +22,23 @@ describe("Social media module", () => {
     const regexResult: RegExpExecArray | null = youtubeRegex.exec(youtubeTestString);
     if (!regexResult) return
     const youtubeLink = new YoutubeLink(regexResult);
-    const spy = jest
-      .spyOn(youtubeLink, "fetchVideos")
-      .mockImplementation(async () => youtubeVideoMockData)
+    const spy = mockYoutubeVideo(youtubeLink);
     const response: string = await youtubeLink.getMessage();
     const expected = "John Walker | Broken [03:19 - 99% - 335.6K]";
     expect(response).toBe(expected)
     spy.mockRestore()
   });
 
+  function mockYoutubeVideo(youtubeLink: YoutubeLink) {
+    return jest
+        .spyOn(youtubeLink, "fetchVideos")
+        .mockImplementation(async () => youtubeVideoMockData);
+  }
+
+  function mockTweet(twitterLink: TwitterLink) {
+    return jest
+        .spyOn(twitterLink, "fetchTweet")
+        .mockImplementation(async () => tweetMockData);
+  }
 })
+
