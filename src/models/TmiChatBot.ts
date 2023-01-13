6b4a49f1f740e0a221a9cceac35834a578117995
Habbe <91habbe@gmail.com>
2023-01-13 14:24:07 +0100

    ♻️ Ref: remove onMessage method

--- src/models/TmiChatBot.ts
+++ src/models/TmiChatBot.ts
@@ -1,31 +1,16 @@
-import { ChatUserstate, Client } from "tmi.js";
-import { tmiOptions } from "../config/tmiConfig";
-import { MessageData } from "./MessageData";
-import { CommandList, StandardCommandList } from "./commands/StandardCommandList";
-import { CustomCommand } from "./commands/CustomCommand";
-import { LinkCommand } from "./commands/LinkCommand";
-import { UserModel } from "./database/UserPrisma";
-import { JoinedUser } from "../interfaces/prisma";
-import channels from "../private/tmi_channels.json";
-import { CommandName } from "../interfaces/tmi";
-import { ClientSingleton } from "./database/ClientSingleton";
 import { ChatEvent } from "./tmi/ChatEvent";
-
+import { ClientSingleton } from "./tmi/ClientSingleton";
 
 export class TmiChatBot {
   private chatEvent = new ChatEvent();
   private client = ClientSingleton.getInstance().get();
 
   constructor() {
-    this.addMessageEvent()
+      this.client.on("message", this.chatEvent.onMessage)
+      this.client.on("join", this.chatEvent.onJoin)
   }
 
   connect(): void {
     this.client.connect().then()
   }
-
-  private addMessageEvent(): void {
-    this.client.on("message", this.chatEvent.onMessage)
-  }
 }
-
