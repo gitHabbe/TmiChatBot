0b43114b4a826026ee03725b868fe8573ad19a26
Habbe <91habbe@gmail.com>
2023-01-13 14:15:31 +0100

    ♻️ Ref: remove and stop use targetChannel field

--- src/models/tmi/MessageData.ts
+++ src/models/tmi/MessageData.ts
@@ -2,11 +2,9 @@ import { ChatUserstate } from "tmi.js";
 
 export class MessageData {
     public channel: string;
-    public targetChannel: string;
     public response: string = "";
 
     constructor(channel: string, public chatter: ChatUserstate, public message: string) {
         this.channel = channel.slice(1);
-        this.targetChannel = channel;
     }
 }
