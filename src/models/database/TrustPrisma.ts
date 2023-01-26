ecf1712ebc5d32d0f7c7831589e7c9a1edb22424
Habbe <91habbe@gmail.com>
2023-01-26 20:35:53 +0100

    🐛 Fix: newTrust name instead of chatter name

--- src/models/database/TrustPrisma.ts
+++ src/models/database/TrustPrisma.ts
@@ -7,13 +7,13 @@ export class TrustModel {
   private db = DatabaseSingleton.getInstance().get();
   private client = this.db[ModelName.trust];
 
-  constructor(private user: JoinedUser, private trustee: Userstate) {}
+  constructor(private user: JoinedUser, private chatter: Userstate) {}
 
   get = (): Promise<Trust | null> => {
     return this.client.findFirst({
       where: {
         userId: this.user.id,
-        name: this.trustee.username,
+        name: this.chatter.username,
       },
     });
   };
@@ -30,23 +30,23 @@ export class TrustModel {
     const isStreamer: boolean = this.user.name.toUpperCase() === deleter.toUpperCase()
     if (isStreamer === false) throw new Error(`Only streamer can remove trust`)
     const hasTrust: Trust | null = await this.get()
-    if (hasTrust === null) throw new Error(`${this.trustee} is not trusted.`)
+    if (hasTrust === null) throw new Error(`${this.chatter} is not trusted.`)
     return this.client.deleteMany({
       where: {
         userId: this.user.id,
-        name: this.trustee.username,
+        name: this.chatter.username,
       },
     });
   };
 
-  save = async (madeBy: string): Promise<Trust> => {
+  save = async (madeBy: string, newTrust: string): Promise<Trust> => {
     const oldTrustee = await this.get()
     if (oldTrustee) throw new Error("Chatter already trusted")
     return this.client.create({
       data: {
         userId: this.user.id,
-        name: this.trustee.username,
-        madeBy: madeBy,
+        name: newTrust,
+        madeBy: this.chatter.username,
       },
     });
   };
