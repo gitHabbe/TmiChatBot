468ca305c2340790016aa2d55cd1b60ce0473aa1
Habbe <91habbe@gmail.com>
2023-01-26 21:29:12 +0100

    ♻️ Ref: cleanup and rearrange

--- src/interfaces/prisma.ts
+++ src/interfaces/prisma.ts
@@ -1,36 +1,5 @@
 import { Category, CategoryLink, Game, GameLink, GameNames, GamePlatform, } from ".prisma/client";
 import { Command, Component, Setting, Timestamp, Trust, User } from "@prisma/client";
-// import { Timestamp, Trust } from "../models/commands/Tmi";
-
-// export interface User {
-//   name: string;
-// }
-
-export interface GameQuery {
-  abbreviation?: string;
-  names?: {
-    international: string;
-  };
-}
-
-export interface UserQuery {
-  id?: string;
-  name?: string;
-}
-
-export interface JoinedGame {
-  id: string;
-  abbreviation: string;
-  names: GameNames | null;
-  links: GameLink[];
-  platforms: GamePlatform[];
-  categories: {
-    links: CategoryLink[];
-    name: string;
-    id: string;
-    gameId: string;
-  }[];
-}
 
 export enum ModelName {
   game = "game",
@@ -48,30 +17,12 @@ export enum ModelName {
   setting = "setting",
 }
 
-export type FullGame = Game & {
-  names: GameNames | null;
-  platforms: GamePlatform[];
-  categories: (Category & {
-    links: CategoryLink[];
-  })[];
-};
-
 export interface Model {
   get(): Promise<any>;
   getAll(): Promise<any[]>;
   save(data: any): Promise<any>;
   delete(): Promise<any>;
 }
-
-// export interface JoinedUser {
-//   user: (User & {
-//     commands: Command[],
-//     components: Component[],
-//     settings: Setting[],
-//     timestamps: Timestamp[],
-//     trusts: Trust[]
-//   });
-// }
 
 export interface JoinedUser extends User {
   // id: string;
@@ -82,3 +33,25 @@ export interface JoinedUser extends User {
   timestamps: Timestamp[],
   trusts: Trust[]
 }
+
+export interface JoinedGame extends Game {
+  id: string;
+  abbreviation: string;
+  names: GameNames | null;
+  links: GameLink[];
+  platforms: GamePlatform[];
+  categories: {
+    links: CategoryLink[];
+    name: string;
+    id: string;
+    gameId: string;
+  }[];
+}
+
+export type FullGame = Game & {
+  names: GameNames | null;
+  platforms: GamePlatform[];
+  categories: (Category & {
+    links: CategoryLink[];
+  })[];
+};
