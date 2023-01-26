b565d6fa03a26e6102113011bacd0545a9c490b7
Habbe <91habbe@gmail.com>
2023-01-26 15:20:12 +0100

    ♻️ Ref: extract logic to more readable methods

--- src/utility/ParseMessage.ts
+++ src/utility/ParseMessage.ts
@@ -10,16 +10,20 @@ export class ParseMessage {
   parseMessage = () => {
     const { message } = this.messageData;
     const supportedVehicles = [ "car", "hover", "plane" ];
-    let query = message.split(" ").slice(2);
+    let query: string[] = message.split(" ").slice(2);
     const specifiedVehicle = query.find((word: string) => supportedVehicles.includes(word));
 
     if (specifiedVehicle) {
-      query = this.filterVehicle(query, specifiedVehicle);
+      query = this.removeVehicleNames(query, specifiedVehicle);
     } else {
-      this.levels = this.levels.filter((track: ITrack) => {
-        return track.default;
-      });
+      this.onlyStandardVehicles();
     }
+    query = this.onlyAbbreviated(query);
+
+    return { query, dkrLevels: this.levels };
+  };
+
+  private onlyAbbreviated(query: string[]): string[] {
     const isAbbreviated = this.levels.filter((track: ITrack) => {
       return query.includes(track.abbreviation);
     });
@@ -27,11 +31,16 @@ export class ParseMessage {
       query = [ isAbbreviated[0].name ];
       this.levels = isAbbreviated;
     }
+    return query;
+  }
 
-    return { query, dkrLevels: this.levels };
-  };
+  private onlyStandardVehicles() {
+    this.levels = this.levels.filter((track: ITrack) => {
+      return track.default;
+    });
+  }
 
-  private filterVehicle(query: string[], specifiedVehicle: string) {
+  private removeVehicleNames(query: string[], specifiedVehicle: string) {
     query = query.filter((word: string) => word !== specifiedVehicle);
     this.levels = this.levels.filter((track: ITrack) => {
       return track.vehicle === specifiedVehicle;
