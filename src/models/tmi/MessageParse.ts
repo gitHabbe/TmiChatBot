3827178294bebcdaa0c7ca7551cd985a5a356015
Habbe <91habbe@gmail.com>
2023-01-26 15:15:39 +0100

    ✨ Add: MessageParser supports more features

--- src/models/tmi/MessageParse.ts
+++ src/models/tmi/MessageParse.ts
@@ -1,6 +1,25 @@
+import { MessageData } from "./MessageData";
+import { TwitchFetch } from "../fetch/TwitchTv";
+
 export class MessageParser {
-    static getCommandName(message: string): string {
-        const chatterCommand: string = message.split(" ")[0];
+    getCommandName(message: string): string {
+        const chatterCommand = this.getWordIndex(message, 0);
         return chatterCommand.slice(1).toUpperCase();
     };
+
+    async getGame(messageData: MessageData, index: number, twitchFetch?: TwitchFetch): Promise<string> {
+        const { message, channel } = messageData;
+        const specifiedGame = this.getWordIndex(message, index)
+        if (!specifiedGame) {
+            twitchFetch = twitchFetch || new TwitchFetch()
+            const twitchChannels = await twitchFetch.channelList(channel)
+            const { game_name } = twitchFetch.filteredChannel(twitchChannels, channel);
+            return game_name;
+        }
+        return specifiedGame
+    }
+
+    getWordIndex(message: string, index: number): string {
+        return message.split(" ")[index];
+    }
 }
