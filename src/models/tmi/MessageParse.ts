bd17e67874629ba3f897d708561d028ea0817da9
Habbe <91habbe@gmail.com>
2023-01-30 13:57:18 +0100

    ✨ Add: more flexibilty in MessageParser

--- src/models/tmi/MessageParse.ts
+++ src/models/tmi/MessageParse.ts
@@ -7,19 +7,33 @@ export class MessageParser {
         return chatterCommand.slice(1).toUpperCase();
     };
 
-    async getGame(messageData: MessageData, index: number, twitchFetch?: TwitchFetch): Promise<string> {
-        const { message, channel } = messageData;
-        const specifiedGame = this.getWordIndex(message, index)
-        if (!specifiedGame) {
-            twitchFetch = twitchFetch || new TwitchFetch()
-            const twitchChannels = await twitchFetch.channelList(channel)
-            const { game_name } = twitchFetch.filteredChannel(twitchChannels, channel);
-            return game_name;
-        }
-        return specifiedGame
-    }
-
     getWordIndex(message: string, index: number): string {
         return message.split(" ")[index];
     }
+
+    private static getSentenceFromIndex(message: string, index: number): string {
+        return message.split(" ").slice(index).join(" ");
+    }
+
+    async gameName(messageData: MessageData, index: number, twitchFetch?: TwitchFetch): Promise<string> {
+        const { message, channel } = messageData;
+        const targetGame = this.getWordIndex(message, index)
+        if (!targetGame) {
+            const fetcher = twitchFetch || new TwitchFetch()
+            const { game_name } = await fetcher.singleChannel(channel);
+            return game_name
+        }
+        return targetGame
+    }
+
+    async categoryName(messageData: MessageData, index: number, twitchFetch?: TwitchFetch): Promise<string> {
+        const { message, channel } = messageData;
+        const targetCategory = MessageParser.getSentenceFromIndex(message, index)
+        if (!targetCategory) {
+            const fetcher= twitchFetch || new TwitchFetch()
+            return fetcher.getTitle(channel);
+        }
+        return targetCategory
+    }
+
 }
