2fc6f5e462e1f0bb2dae5c33ff0ff124b6e31c29
Habbe <91habbe@gmail.com>
2023-03-14 15:52:34 +0100

    🚧 Wip: debugging tool

--- test/ChatEventDebug.ts
+++ test/ChatEventDebug.ts
@@ -11,22 +11,35 @@ interface TestResponse {
     channel: string;
     response: string
 }
+
 (async function() {
     const chatEvent = new ChatEvent()
     const clientSingleton = ClientSingleton.getInstance()
     // @ts-ignore
     const fakeClient: TmiClientTest = {
         testResults: {
-            channel: "placeholder channel",
-            response: "placeholder response"
+            channel: "no channel",
+            response: "no response"
         },
         say: function (channel, response) {
             this.testResults = { channel, response }
         },
+        part: async function(channel: string): Promise<[ string ]> {
+            this.testResults = {
+                channel: channel,
+                response: "leaving channel " + channel
+            }
+            return [""]
+        }
     };
+
+    const start = Date.now();
+
     clientSingleton.client = fakeClient
-    await chatEvent.onMessage("#gage", chatterMock, "!wr", false)
+    await chatEvent.onMessage("#habbe", chatterMock, "!newcmd !ppog very pog command", false)
     // @ts-ignore
-    const res = clientSingleton.client.testResults.response;
-    console.log(res);
+    const { response, channel } = clientSingleton.client.testResults;
+    const end = Date.now();
+    console.log(`Bot response: ${response}`);
+    console.log(`To channel: ${channel}, in: ${end - start} ms`);
 })();
