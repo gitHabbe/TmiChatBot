44e5e002eee43dfb9c3c0e89d9a5287e970dc1df
Habbe <91habbe@gmail.com>
2023-03-14 15:27:40 +0100

    ✨ Add: json class for tmijs channels

--- src/models/JsonArrayFile.ts
+++ src/models/JsonArrayFile.ts
@@ -1,24 +1,24 @@
-import { readFileSync } from "fs";
-import {
-  ITrack,
-  ILevels,
-  ITimeTrialJson,
-  ITimeTrialsJson,
-} from "../interfaces/specificGames";
+import { readFileSync } from "fs"
+import { ILevels, ITimeTrialJson, ITimeTrialsJson, ITrack, } from "../interfaces/specificGames"
+import * as fs from "fs"
 
 export class JsonFile<T> {
-  jsonFile: string = `./src/utility/${this.filename}.json`;
+  jsonFile: string = `./src/${this.path}${this.filename}.json`;
   private data = JSON.parse(readFileSync(this.jsonFile, "utf8"));
 
-  constructor(private filename: string) {}
+  constructor(private path: string, private filename: string) {}
 
   get getData(): T {
     return this.data;
   }
+
+  set setData(data: T) {
+    this.data = data;
+  }
 }
 
 export class JsonLevels {
-  private levels = new JsonFile<ILevels>("dkr_data");
+  private levels = new JsonFile<ILevels>("utility/", "dkr_data");
 
   data(): ITrack[] {
     return this.levels.getData.levels;
@@ -32,7 +32,7 @@ export class JsonLevels {
 }
 
 export class JsonTimeTrials {
-  private levels = new JsonFile<ITimeTrialsJson>("dkr_data");
+  private levels = new JsonFile<ITimeTrialsJson>("utility/", "dkr_data");
 
   data(): ITimeTrialJson[] {
     return this.levels.getData.timetrial;
@@ -43,4 +43,23 @@ export class JsonTimeTrials {
       return level.abbreviation === lvl.abbreviation;
     });
   };
+}
+
+export class JsonChannels {
+  private channels = new JsonFile<string[]>(this.path, this.filename);
+  constructor(private path: string, private filename: string) {}
+
+  data(): string[] {
+    return this.channels.getData
+  }
+
+  add(name: string) {
+    this.channels.getData.push(name.toLowerCase());
+    fs.writeFileSync(this.channels.jsonFile, JSON.stringify(this.channels.getData));
+  }
+
+  remove(name: string) {
+    this.channels.setData = this.channels.getData.filter((channel: string) => channel.toLowerCase() !== name.toLowerCase());
+    fs.writeFileSync(this.channels.jsonFile, JSON.stringify(this.channels.getData));
+  }
 }
