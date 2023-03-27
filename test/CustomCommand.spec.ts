f6ef4187ed97665e8724011dd4bfc6ab4b4a8bf9
Habbe <91habbe@gmail.com>
2023-03-27 12:51:56 +0200

    🐛 Fix: adapt test to new CustomCommand structure

--- test/CustomCommand.spec.ts
+++ test/CustomCommand.spec.ts
@@ -2,15 +2,18 @@ import { CustomCommand } from "../src/models/commands/CustomCommand";
 import { Command } from "@prisma/client";
 import { commandMockData } from "./mockData";
 import { MessageData } from "../src/models/tmi/MessageData";
+import { UserPrisma } from "../src/models/database/UserPrisma"
 
 describe("Test CustomCommand", () => {
 
   it("Init CustomCommand", async () => {
     const messageData = new MessageData("habbe", {}, "This is a test message");
-    const customCommand = new CustomCommand(messageData);
+    const userPrisma = new UserPrisma("habbe")
+    const joinedUser = await userPrisma.get()
+    const customCommand = new CustomCommand(messageData, joinedUser);
     const spy = jest
       .spyOn(customCommand, "isCustomCommand")
-      .mockImplementation(async (): Promise<Command> => commandMockData)
+      .mockImplementation((): Command | undefined => commandMockData)
     const response = "This is the content";
     const expected: MessageData = await customCommand.run();
     expect(expected.response).toBe(response)
