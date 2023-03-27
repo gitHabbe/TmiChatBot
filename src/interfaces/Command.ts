d2ec549f29f78ebc3eb0412f45d96d472e76c903
Habbe <91habbe@gmail.com>
2023-03-27 12:51:10 +0200

    🚧 Wip: experiment with new command interface

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,9 +1,14 @@
 import { MessageData } from "../models/tmi/MessageData";
 import { ModuleFamily } from "./tmi";
+import { JoinedUser } from "./prisma"
 
 export interface ICommand {
     messageData: MessageData;
     moduleFamily: ModuleFamily;
 
     run(): Promise<MessageData>;
+}
+
+export interface ICommandUser extends ICommand {
+    user: JoinedUser;
 }
