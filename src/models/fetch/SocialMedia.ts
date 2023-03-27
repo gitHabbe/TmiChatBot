9fb1dcda908c2e94458e1849aa8553da16faf9e6
Habbe <91habbe@gmail.com>
2023-03-27 12:44:42 +0200

    ♻️ Ref: less code inside try catch block

--- src/models/fetch/SocialMedia.ts
+++ src/models/fetch/SocialMedia.ts
@@ -3,7 +3,6 @@ import { twitterAPI, twitterRegex } from "../../config/twitterConfig";
 import { ITwitterTweet, ITwitterTweetResponse, ITwitterUser, IYoutubePagination, } from "../../interfaces/socialMedia";
 import {
   numberToRoundedWithLetter,
-  stringToProbabilityPercent,
   youtubeDurationToHHMMSS,
 } from "../../utility/dateFormat";
 
@@ -15,15 +14,15 @@ interface Link {
 export class LinkParser {
   constructor(private message: string) {}
 
-  async matchRegex(): Promise<string> {
-    let regex = youtubeRegex.exec(this.message);
-    if (regex) {
-      const youtubeLink = new YoutubeLink(regex);
+  async getLinkMessage(): Promise<string> {
+    const youtubeRegexResult = youtubeRegex.exec(this.message)
+    if (youtubeRegexResult) {
+      const youtubeLink = new YoutubeLink(youtubeRegexResult);
       return await youtubeLink.getMessage();
     }
-    regex = twitterRegex.exec(this.message);
-    if (regex) {
-      const twitterLink = new TwitterLink(regex);
+    const twitterRegexResults = twitterRegex.exec(this.message);
+    if (twitterRegexResults) {
+      const twitterLink = new TwitterLink(twitterRegexResults);
       return await twitterLink.getMessage();
     }
     return "";
@@ -32,27 +31,27 @@ export class LinkParser {
 
 export class TwitterLink implements Link {
   constructor(public regex: RegExpExecArray) {}
+
   async getMessage(): Promise<string> {
-    try {
-      const tweet = await this.fetchTweet();
-      const { text }: ITwitterTweet = tweet.data;
-      const { name, username, verified }: ITwitterUser =
-        tweet.includes.users[0];
-      const verifiedCheck: string = verified ? "✔" : ""; // alts: ✅ 💬
-
-      return `[${name}@${username}${verifiedCheck}] ${text}`;
-    } catch (error: any) {
-      if (error.message) return error.message;
-      return "";
-    }
-  };
-
-  async fetchTweet() {
     const tweet_id = this.regex[1];
     const tweetFieldsQuery = `expansions=author_id&user.fields=name,username,verified&tweet.fields=public_metrics,created_at`;
-    const query = `/tweets/${tweet_id}?${tweetFieldsQuery}`;
-    const { data } = await twitterAPI.get<ITwitterTweetResponse>(query);
-    return data;
+    const query = `/tweets/${tweet_id}?${tweetFieldsQuery}`
+    const tweet = await this.fetchTweet(query);
+    const { text }: ITwitterTweet = tweet.data;
+    const { name, username, verified }: ITwitterUser =
+      tweet.includes.users[0];
+    const verifiedCheck: string = verified ? "✔" : ""; // alts: ✅ 💬
+
+    return `[${name}@${username}${verifiedCheck}] ${text}`;
+  };
+
+  async fetchTweet(query: string) {
+    try {
+      const { data } = await twitterAPI.get<ITwitterTweetResponse>(query)
+      return data;
+    } catch {
+      throw new Error("Error getting tweet info")
+    }
   };
 }
 
@@ -60,26 +59,25 @@ export class YoutubeLink implements Link {
   constructor(public regex: RegExpExecArray) {}
 
   async getMessage(): Promise<string> {
-    try {
-      const videos = await this.fetchVideos();
-      const video = videos.items[0];
-      const { viewCount, likeCount } = video.statistics;
-      const videoDuration = youtubeDurationToHHMMSS(video.contentDetails.duration);
-      const views = numberToRoundedWithLetter(viewCount);
-      const title = video.snippet.title;
-      const videoLikeCount = numberToRoundedWithLetter(likeCount)
+    const youtube_id = this.regex[3];
+    const query = `/videos?id=${youtube_id}&key=${process.env.YOUTUBE_API_KEY}&part=snippet,contentDetails,statistics,status`
+    const videos = await this.fetchVideos(query);
+    const video = videos.items[0];
+    const { viewCount, likeCount } = video.statistics;
+    const videoDuration = youtubeDurationToHHMMSS(video.contentDetails.duration);
+    const views = numberToRoundedWithLetter(viewCount);
+    const title = video.snippet.title;
+    const videoLikeCount = numberToRoundedWithLetter(likeCount)
 
-      return `${title} [${videoDuration} - ${videoLikeCount}👍 - ${views}]`;
-    } catch (error: any) {
-      if (error) return error.message;
-      return "";
-    }
+    return `${title} [${videoDuration} - ${videoLikeCount}👍 - ${views}]`;
   };
 
-  async fetchVideos(): Promise<IYoutubePagination> {
-    const youtube_id = this.regex[3];
-    const query = `/videos?id=${youtube_id}&key=${process.env.YOUTUBE_API_KEY}&part=snippet,contentDetails,statistics,status`;
-    const { data } = await youtubeAPI.get<IYoutubePagination>(query);
-    return data;
+  async fetchVideos(query: string): Promise<IYoutubePagination> {
+    try {
+      const { data } = await youtubeAPI.get<IYoutubePagination>(query);
+      return data;
+    } catch {
+      throw new Error("Error getting youtube video info")
+    }
   }
 }
