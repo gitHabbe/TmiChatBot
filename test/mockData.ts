224be66e600f82e8104b29fb3ada202a16f07612
Habbe <91habbe@gmail.com>
2023-03-30 10:20:04 +0200

    🔥 Del: unused mock file
    🔥 Del: squash

--- test/mockData.ts
+++
@@ -1,5 +0,0 @@
-import { MessageData } from "../src/models/tmi/MessageData"
-
-
-export const messagePokemonDataMock = new MessageData("#habbe", {}, "!pokemonmove slash");
-export const messagePokemonMachineDataMock = new MessageData("#habbe", {}, "!pokemontm TM46");
