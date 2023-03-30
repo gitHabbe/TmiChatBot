b767cbbf5f3b90538bcbc7e3fd314b58c112682a
Habbe <91habbe@gmail.com>
2023-03-30 08:46:17 +0200

    ✅ Test: pokemon HM command

--- test/PokemonAPI.spec.ts
+++ test/PokemonAPI.spec.ts
@@ -1,7 +1,8 @@
-import { messagePokemonDataMock, pokemonItemMock, pokemonMoveMock } from "./mockData"
+import { messagePokemonDataMock, messagePokemonMachineDataMock, pokemonItemMock, pokemonMoveMock } from "./mockData"
 import { PokemonAPI } from "../src/models/fetch/PokemonAPI"
 import { PokemonMoveImpl } from "../src/models/commands/pokemon/PokemonMoveImpl"
 import { PokemonItemImpl } from "../src/models/commands/pokemon/PokemonItemImpl"
+import { PokemonHM } from "../src/models/commands/pokemon/PokemonHM"
 
 describe("PokemonAPI module", () => {
 
@@ -28,6 +29,20 @@ describe("PokemonAPI module", () => {
         const res = messageData.response
         const exp = "Poké Ball[200]: Attempts to catch a wild Pokémon, using a catch rate of 1×. If used in a trainer battle, nothing happens and the ball is lost."
         expect(res).toBe(exp)
+        spy.mockRestore()
+    })
+
+    it("Pokemon Machine", async () => {
+
+        const mockedPokemonAPI = new PokemonAPI()
+        const pokemonMachineMock: Record<string, string> = {
+            "HM01": "Teaches the move Cut."
+        }
+        const spy = jest
+            .spyOn(mockedPokemonAPI, "fetchMachine")
+            .mockReturnValue(pokemonMachineMock)
+        const pokemonMachine = new PokemonHM(messagePokemonMachineDataMock, mockedPokemonAPI)
+        const messageDataPromise = await pokemonMachine.run()
         spy.mockRestore()
     })
 
