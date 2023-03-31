2d095832ba9ffe91cab3eb177126f39348db4804
Habbe <91habbe@gmail.com>
2023-03-31 10:10:09 +0200

    ✅ Test: StandardCommandMap

---
+++ test/StandardCommandMap.spec.ts
@@ -0,0 +1,37 @@
+import { StandardCommandMap } from "../src/models/commands/StandardCommandMap"
+import { messageDataMock } from "./__mocks__/tmiMock"
+import { joinedUserMock } from "./__mocks__/prismaMock"
+import { TwitchTitle } from "../src/models/commands/twitch/TwitchTitle"
+import { CommandName, ComponentsSupport } from "../src/interfaces/tmi"
+import { Pokemon } from "../src/models/commands/pokemon/Pokemon"
+import { MessageParser } from "../src/models/tmi/MessageParse"
+import { ICommand } from "../src/interfaces/Command"
+
+describe("StandardCommandMap module", () => {
+    let standardCommandMap: StandardCommandMap
+
+    beforeEach(() => {
+        standardCommandMap = new StandardCommandMap(messageDataMock, joinedUserMock)
+    })
+
+    it("StandardCommandMap title", () => {
+        const res: ICommand | undefined = getCommand("!title")
+        if (!res) expect(res).toBeNull()
+        expect(res).toBeInstanceOf(TwitchTitle)
+    })
+
+    it("StandardCommandMap pokemon", () => {
+        const res: ICommand | undefined = getCommand("!pokemon")
+        if (!res) expect(res).toBeNull()
+        expect(res).toBeInstanceOf(Pokemon)
+    })
+
+    function getCommand(mockMessage: string) {
+        messageDataMock.message = mockMessage
+        const { message } = messageDataMock
+        const messageParser = new MessageParser()
+        const commandName: string = messageParser.getCommandName(message, "!")
+        return standardCommandMap.get(commandName)
+    }
+
+})
