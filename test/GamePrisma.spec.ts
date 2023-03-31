781a05da0bdc625914a1c807f228769058be7e3d
Habbe <91habbe@gmail.com>
2023-03-31 09:11:27 +0200

    🐛 Fix: incomplete test

--- test/GamePrisma.spec.ts
+++ test/GamePrisma.spec.ts
@@ -4,12 +4,17 @@ import { prismaSpeedrunGameMock } from "./__mocks__/prismaMock"
 
 describe("GamePrisma module", () => {
 
-    it("GamePrisma init", async () => {
+    async function createSpy() {
         const gamePrisma = new GamePrisma("habbe")
         const spy = jest
             .spyOn(gamePrisma, "get")
             .mockReturnValue(Promise.resolve(prismaSpeedrunGameMock))
         const fullSpeedrunGame: FullSpeedrunGame | null = await gamePrisma.get()
+        return { spy, fullSpeedrunGame }
+    }
+
+    it("GamePrisma category id", async () => {
+        const { spy, fullSpeedrunGame } = await createSpy()
         if (!fullSpeedrunGame) {
             expect(fullSpeedrunGame).toBeNull()
         }
@@ -17,6 +22,19 @@ describe("GamePrisma module", () => {
         const targetCategoryId = fullSpeedrunGame?.categories.find(category => category.name === targetCategory)
         const res = targetCategoryId?.id
         const exp = "q25qqv2o"
+        expect(res).toBe(exp)
+        spy.mockRestore()
+    })
+
+    it("GamePrisma category name", async () => {
+        const { spy, fullSpeedrunGame } = await createSpy()
+        if (!fullSpeedrunGame) {
+            expect(fullSpeedrunGame).toBeNull()
+        }
+        const targetCategory = "q25qqv2o"
+        const targetCategoryId = fullSpeedrunGame?.categories.find(category => category.id === targetCategory)
+        const res = targetCategoryId?.name
+        const exp = "100%"
         expect(res).toBe(exp)
         spy.mockRestore()
     })
