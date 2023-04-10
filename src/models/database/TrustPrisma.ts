67c03b2797da4f9062557705c52baec36fe5fb51
Habbe <91habbe@gmail.com>
2023-04-10 10:35:28 +0200

    🐛 Fix: wrong interface in class

--- src/models/database/TrustPrisma.ts
+++ src/models/database/TrustPrisma.ts
@@ -1,13 +1,13 @@
 import { Trust } from "@prisma/client";
 import { JoinedUser, ModelName } from "../../interfaces/prisma";
 import { DatabaseSingleton } from "./Prisma";
-import { Userstate } from "tmi.js";
+import { ChatUserstate } from "tmi.js"
 
 export class TrustPrisma {
   private db = DatabaseSingleton.getInstance().get();
   private client = this.db[ModelName.trust];
 
-  constructor(private user: JoinedUser, private chatter: Userstate) {}
+  constructor(private user: JoinedUser, private chatter: ChatUserstate) {}
 
   get(trustee: string) {
     return this.client.findFirst({
@@ -42,6 +42,7 @@ export class TrustPrisma {
   async save(madeBy: string, newTrust: string): Promise<Trust> {
     const oldTrustee = await this.get(newTrust)
     if (oldTrustee) throw new Error("Chatter already trusted")
+    if (!this.chatter.username) throw new Error("No chatter")
     return this.client.create({
       data: {
         userId: this.user.id,
